8eaba0f637e2903b538ec93ba4abd713d79d7ea6
Jerry Alania <alanjerr@isu.edu>
2026-04-29 21:09:59 -0600

    Fixed light mode colors

--- app/src/main/java/com/example/studentproductivityapp/ScheduleActivity.kt
+++ app/src/main/java/com/example/studentproductivityapp/ScheduleActivity.kt
@@ -58,19 +58,45 @@ class ScheduleActivity  : AppCompatActivity() {
         rvCompleted.layoutManager = LinearLayoutManager(this)
 
         //Swipe-to-delete functionality
+// Swipe-to-delete functionality with Red Background and Icon
         val swipeToDeleteCallback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
             override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder) = false
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                // Get the item that was swiped
                 val position = viewHolder.bindingAdapterPosition
                 val currentAdapter = viewHolder.bindingAdapter as? AssignmentAdapter
                 val assignmentToDelete = currentAdapter?.currentList?.getOrNull(position)
 
-                // Delete from Room Database
                 assignmentToDelete?.let {
                     viewModel.delete(it)
                 }
+            }
+
+            override fun onChildDraw(
+                c: android.graphics.Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean
+            ) {
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+
+                val itemView = viewHolder.itemView
+                val background = android.graphics.drawable.ColorDrawable(android.graphics.Color.parseColor("#EF5350")) // Material Red
+
+                // You can add a trash can icon if you have one in your drawable folder!
+                // val icon = androidx.core.content.ContextCompat.getDrawable(this@ScheduleActivity, android.R.drawable.ic_menu_delete)
+
+                if (dX > 0) { // Swiping to the right
+                    background.setBounds(itemView.left, itemView.top, itemView.left + dX.toInt(), itemView.bottom)
+                } else if (dX < 0) { // Swiping to the left
+                    background.setBounds(itemView.right + dX.toInt(), itemView.top, itemView.right, itemView.bottom)
+                } else { // view is unSwiped
+                    background.setBounds(0, 0, 0, 0)
+                }
+                background.draw(c)
             }
         }
 
